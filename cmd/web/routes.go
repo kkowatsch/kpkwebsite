0d7ce4d33c909e0029aebc5ef80aeca41a152cbb
kkowatsch <kayleigh.kowatsch@gmail.com>
2023-03-11 12:23:52 -0500

    working routes with middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,13 +10,16 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
-	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		app.notFound(w)
-	})
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	router.Handler(http.MethodGet, "/static/", http.StripPrefix("/static", fileServer))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
-
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/kpk-about", dynamic.ThenFunc(app.about))
+	router.Handler(http.MethodGet, "/kpk-services", dynamic.ThenFunc(app.services))
+	router.Handler(http.MethodGet, "/kpk-contact", dynamic.ThenFunc(app.contact))
+	router.Handler(http.MethodGet, "/kpk-contactform", dynamic.ThenFunc(app.contactFormPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest)
 	return standard.Then(router)
