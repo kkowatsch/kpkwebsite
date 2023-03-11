02a3e1ae19250a6f5402de2c49e3ce832768b9c6
kkowatsch <kayleigh.kowatsch@gmail.com>
2023-03-11 12:23:52 -0500

    working routes with middleware

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,35 +1,56 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	data := app.newTemplateData(r)
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	app.render(w, http.StatusOK, "home.tmpl", data)
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.errorLog.Print(err.Error())
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.errorLog.Print(err.Error())
+		app.serverError(w, err)
+	}
 }
 
-func about(w http.ResponseWriter, r *http.Request) {
+func (app *application) about(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("About KPK Accounting"))
 
 }
 
-func services(w http.ResponseWriter, r *http.Request) {
+func (app *application) services(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Services offered from KPK Accounting"))
 
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func (app *application) contact(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Contact KPK Accounting"))
 }
 
-func contactformpost(w http.ResponseWriter, r *http.Request) {
+func (app *application) contactFormPost(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Contact Form sending..."))
+
+	w.Write([]byte("Create a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -13,45 +12,32 @@ import (
 type application struct {
 	errorLog       *log.Logger
 	infoLog        *log.Logger
-	templateCache  map[string]*template.Template
 	sessionManager *scs.SessionManager
 }
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	templateCache, err := newTemplateCache()
-	if err != nil {
-		errorLog.Println("Error creating template")
-		errorLog.Fatal(err)
-	}
-
 	sessionManager := scs.New()
-
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/services", services)
-	http.HandleFunc("/contact", contact)
-	// sends email to kpkaccounting@___.com
-	http.HandleFunc("/contact/form", contactformpost)
 
 	app := &application{
 		errorLog:       errorLog,
 		infoLog:        infoLog,
-		templateCache:  templateCache,
 		sessionManager: sessionManager,
 	}
 
 	srv := &http.Server{
-		Addr:     "localhost:4000",
+		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(),
 	}
 
-	log.Printf("Starting server on 4000")
-	if err = srv.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
