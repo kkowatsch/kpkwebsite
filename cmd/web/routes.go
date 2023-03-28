028589e2f2e0fc5c67aebded414a68020864c509
kkowatsch <kayleigh.kowatsch@gmail.com>
2023-03-28 17:23:53 -0400

    Introducing errors on contact page at this time. Pushing as placeholder to work through bugs.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,8 +23,7 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/about", dynamic.ThenFunc(app.about))
 	router.Handler(http.MethodGet, "/services", dynamic.ThenFunc(app.services))
 	router.Handler(http.MethodGet, "/contact", dynamic.ThenFunc(app.contact))
-	router.Handler(http.MethodPost, "/contactform", dynamic.ThenFunc(app.contactFormPost))
-	router.Handler(http.MethodGet, "/contactformsuccess", dynamic.ThenFunc(app.contactFormSuccess))
+	router.Handler(http.MethodPost, "/contact", dynamic.ThenFunc(app.contactFormPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest)
 	return standard.Then(router)
