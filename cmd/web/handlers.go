292b4c35f67d1a4198834f707f85cec09cff1ad3
kkowatsch <kayleigh.schlupp@gmail.com>
2023-05-14 23:30:15 -0400

    improving SEO

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,8 @@ import (
 )
 
 type PageParams struct {
-	Title string
+	Title       string
+	Description string
 }
 
 type contactForm struct {
@@ -41,7 +42,7 @@ func (form *contactForm) Deliver() error {
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
-	data.Page = PageParams{Title: "Home"}
+	data.Page = PageParams{Title: "Home", Description: "The KPK Accounting Inc. home page with quick highlights of the services provided, the story of KPK's CEO and quick access for submitting inquiries through the contact form."}
 	data.Form = contactForm{}
 
 	app.render(w, http.StatusOK, "home.tmpl", data)
@@ -66,14 +67,14 @@ func (app *application) homeContactFormPost(w http.ResponseWriter, r *http.Reque
 		app.infoLog.Println("form not valid.")
 		data := app.newTemplateData(r)
 		data.Form = form
-		data.Page = PageParams{Title: "Home"}
+		data.Page = PageParams{Title: "Home", Description: "The KPK Accounting Inc. home page with quick highlights of the services provided, the story of KPK's CEO and quick access for submitting inquiries through the contact form."}
 		app.render(w, http.StatusUnprocessableEntity, "home.tmpl", data)
 		return
 	}
 
 	data := app.newTemplateData(r)
 	data.Form = form
-	data.Page = PageParams{Title: "Confirmation"}
+	data.Page = PageParams{Title: "Confirmation", Description: "This page is visible because an inquiry through the contact form has been submitted to KPK Accounting Inc."}
 	app.render(w, http.StatusOK, "confirmation.tmpl", data)
 
 	if err := form.Deliver(); err != nil {
@@ -88,7 +89,7 @@ func (app *application) homeContactFormPost(w http.ResponseWriter, r *http.Reque
 
 func (app *application) about(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
-	data.Page = PageParams{Title: "About"}
+	data.Page = PageParams{Title: "About", Description: "The story of the KPK Accounting Inc. CEO, Ken Kowatsch, and his approach to providing business owners, entrepreneurs and start-ups with the tools for long-term financial health."}
 
 	app.render(w, http.StatusOK, "about.tmpl", data)
 
@@ -96,7 +97,7 @@ func (app *application) about(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) services(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
-	data.Page = PageParams{Title: "Services"}
+	data.Page = PageParams{Title: "Services", Description: "The services provided by KPK Accounting Inc. to provide long-term financial health. Some services offered are Cash Flow Optimization, Audit Leadership, Risk Identification, KPI Reporting Implementation, etc."}
 
 	app.render(w, http.StatusOK, "services.tmpl", data)
 
@@ -104,7 +105,7 @@ func (app *application) services(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) contact(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
-	data.Page = PageParams{Title: "Contact"}
+	data.Page = PageParams{Title: "Contact", Description: "The KPK Accounting Inc. contact form where clients can easily connect with KPK Accounting regarding financial inquiries."}
 	data.Form = contactForm{}
 
 	app.render(w, http.StatusOK, "contact.tmpl", data)
@@ -129,14 +130,14 @@ func (app *application) contactFormPost(w http.ResponseWriter, r *http.Request) 
 		app.infoLog.Println("form not valid.")
 		data := app.newTemplateData(r)
 		data.Form = form
-		data.Page = PageParams{Title: "Contact"}
+		data.Page = PageParams{Title: "Contact", Description: "The KPK Accounting Inc. contact form where clients can easily connect with KPK Accounting regarding financial inquiries."}
 		app.render(w, http.StatusUnprocessableEntity, "contact.tmpl", data)
 		return
 	}
 
 	data := app.newTemplateData(r)
 	data.Form = form
-	data.Page = PageParams{Title: "Confirmation"}
+	data.Page = PageParams{Title: "Confirmation", Description: "This page is visible because an inquiry through the contact form has been submitted to KPK Accounting Inc."}
 	app.render(w, http.StatusOK, "confirmation.tmpl", data)
 
 	if err := form.Deliver(); err != nil {
